5c45d83fee53e75a24f6e203d3931340cd880dc5
Brian Tang <ashrielbrian@gmail.com>
2021-08-07 16:20:07 +0800

    Adds fastapi

--- web/app.py
+++ web/app.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI
+import os
+from io import StringIO
+import pandas as pd
+from fastapi import FastAPI, UploadFile, File
 from typing import Union, Optional
 from pydantic import BaseModel
+from model.ml.model import RFModel
+from model.ml.data import process_data
+from .census_class import CensusClass
 
 class InferenceInput(BaseModel):
     pass
+
+# model_artifact_dir = os.path.join(os.path.dirname(__file__), '..', 'artifact', 'model')
+# model       = load_artifact(os.path.join(model_artifact_dir, 'random_forest.pkl'))
+# binarizer   = load_artifact(os.path.join(model_artifact_dir, 'label_binarizer.pkl'))
+# encoder     = load_artifact(os.path.join(model_artifact_dir, 'oh_encoder.pkl'))
+
+rf_model = RFModel()
 
 app = FastAPI()
 
@@ -11,6 +24,54 @@ app = FastAPI()
 def welcome():
     return "Hello from Census Predictor!"
 
+@app.post('/batch_inference')
+async def batch_inference(csv_file: UploadFile = File(...)):
+
+    try:
+        # loads the uploaded file
+        str_buf = StringIO(str(csv_file.file.read(), 'utf-8'))
+        df = pd.read_csv(str_buf, encoding='utf-8')
+        df.columns = [col.strip().replace('-', '_') for col in df.columns]
+
+    except Exception as e:
+        print(str(e))
+        return {'success': False, 'results': None, 'error': 'Failed to parse uploaded file.'}
+
+    try:
+        # process and perform inference
+        X, _, _, _ = process_data(df, 
+                                categorical_features=rf_model.CAT_FEATURES, 
+                                training=False, 
+                                encoder=rf_model.encoder, 
+                                lb=rf_model.binarizer)
+        y_preds = rf_model.inference(X)
+    except Exception as e:
+        print(str(e))
+        return {'success': False, 'results': None, 'error': 'Failed to perform inference.'}
+    
+    return {'success': True, 'results': y_preds, 'error': None}
+
 @app.post('/inference')
-def inference():
-    pass
+async def inference(individual: CensusClass):
+
+    try:
+        indv_dict = {k: [v] for k,v in individual.dict().items()}
+        df = pd.DataFrame.from_dict(indv_dict)
+    except Exception as e:
+        print(str(e))
+        return {'success': False, 'results': None, 'error': 'Failed to parse JSON body.'}
+    
+    try:
+        # process and perform inference
+        X, _, _, _ = process_data(df, 
+                                categorical_features=rf_model.CAT_FEATURES, 
+                                training=False, 
+                                encoder=rf_model.encoder, 
+                                lb=rf_model.binarizer)
+        y_preds = rf_model.inference(X)
+    except Exception as e:
+        print(str(e))
+        return {'success': False, 'results': None, 'error': 'Failed to perform inference.'}
+    
+    return {'success': True, 'results': y_preds, 'error': None}
+    
---
+++ web/census_class.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel
+
+class CensusClass(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: str
+    education: str
+    education_num: int
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int
+    capital_loss: int
+    hours_per_week: int
+    native_country: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "age": 39,
+                "workclass": "State-gov",
+                "fnlgt": "77516",
+                "education": "Bachelors",
+                "education_num": 13,
+                "marital_status": "Never-married",
+                "occupation": "Adm-clerical",
+                "relationship": "Not-in-family",
+                "race": "White",
+                "sex": "Male",
+                "capital_gain": 2174,
+                "capital_loss": 0,
+                "hours_per_week": 40,
+                "native_country": "United-States"
+            }
+        }
